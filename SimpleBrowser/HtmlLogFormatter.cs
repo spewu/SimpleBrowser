ac6a5230cf17596cc21a9539f2a25d7e1dfd2ca2
spe <sh.pedersen@gmail.com>
2013-05-30 18:43:08 +0200

    Some formatting

--- SimpleBrowser/HtmlLogFormatter.cs
+++ SimpleBrowser/HtmlLogFormatter.cs
@@ -1,33 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using RazorHosting;
-using SimpleBrowser.Properties;
-
-namespace SimpleBrowser
+﻿namespace SimpleBrowser
 {
-	public class HtmlLogFormatter
-	{
-		public class RazorModel
-		{
-			public DateTime CaptureDate { get; set; }
-			public string Title { get; set; }
-			public TimeSpan TotalDuration { get; set; }
-			public List<LogItem> Logs { get; set; }
-			public int RequestsCount { get; set; }
-		}
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using RazorHosting;
+    using SimpleBrowser.Properties;
 
-		public string Render(List<LogItem> logs, string title)
-		{
-			var engine = new RazorEngine<RazorTemplateBase>();
-			var html = engine.RenderTemplate(Resources.HtmlLogTemplate, new[] { typeof(Browser).Assembly.Location, "System.Web.dll" }, new RazorModel {
-				CaptureDate = DateTime.UtcNow,
-				TotalDuration = logs.Count == 0 ? TimeSpan.MinValue : logs.Last().ServerTime - logs.First().ServerTime,
-				Title = title,
-				Logs = logs,
-				RequestsCount = logs.Count(l => l is HttpRequestLog)
-			});
-			return html ?? engine.ErrorMessage;
-		}
-	}
+    public class HtmlLogFormatter
+    {
+        public class RazorModel
+        {
+            public DateTime CaptureDate { get; set; }
+
+            public string Title { get; set; }
+
+            public TimeSpan TotalDuration { get; set; }
+
+            public List<LogItem> Logs { get; set; }
+
+            public int RequestsCount { get; set; }
+        }
+
+        public string Render(List<LogItem> logs, string title)
+        {
+            var engine = new RazorEngine<RazorTemplateBase>();
+            var html = engine.RenderTemplate(Resources.HtmlLogTemplate, new[] { typeof(Browser).Assembly.Location, "System.Web.dll" }, new RazorModel
+            {
+                CaptureDate = DateTime.UtcNow,
+                TotalDuration = logs.Count == 0 ? TimeSpan.MinValue : logs.Last().ServerTime - logs.First().ServerTime,
+                Title = title,
+                Logs = logs,
+                RequestsCount = logs.Count(l => l is HttpRequestLog)
+            });
+
+            return html ?? engine.ErrorMessage;
+        }
+    }
 }
